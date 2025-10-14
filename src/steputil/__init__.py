dc6e67b1f642dc7d3935fce1d27dadd1c0c2dec0
Jan Kneissler <jan.kneissler@breuninger.de>
2025-10-14 22:29:32 +0200

    builder pattern for args

--- src/steputil/__init__.py
+++ src/steputil/__init__.py
@@ -1,7 +1,8 @@
 """steputil - Utilities to simplify creation of pipeline steps."""
 
 from steputil.info import info
+from steputil.argparser import StepArgsBuilder, StepArgs, InputField, OutputField
 
 __version__ = "0.1.0"
 
-__all__ = ["info", "__version__"]
+__all__ = ["info", "__version__", "StepArgsBuilder", "StepArgs", "InputField", "OutputField"]
